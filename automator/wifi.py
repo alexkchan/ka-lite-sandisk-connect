aa8343bbdf936cf2bcca38421e769f0d2c3cd139
Alex Chan <alexkchan@gmail.com>
2015-02-20 18:21:56 -0800

    Integrated Alex code with, started new controller.py and moved sandisk wipi scanning functions into wipi.py

---
+++ automator/wifi.py
@@ -0,0 +1,32 @@
+def get_server_list():
+	#scan for wifi
+	proc = subprocess.Popen('sudo wlist wlan0 scan 2>/dev/null', shell=True, stdout=subprocess.PIPE,)
+	#displays outputs of list
+	stdout_str = proc.communicate()[0]
+	#parse the outputs
+	stdout_list = stdout_str.split('\n')
+	#empty array to save the parsed outputs
+	server_ids=[]
+	#search for wifi server_ids that match SanDisk Media
+	for line in stdout_list:
+		line=line.strip()
+		match=re.search('SanDisk Media (\w\w\w\w)', line)
+		if match:
+			server_ids.append(match.group(1))
+	return server_ids
+
+def get_wipi_list():
+	proc = subprocess.Popen('sudo ifconfig 2>/dev/null', shell=True, stdout=subprocess.PIPE,)
+	#displays outputs of list
+	stdout_str = proc.communicate()[0]
+	#parse the outputs
+	stdout_list = stdout_str.split('\n')
+	#empty array to save the parsed outputs
+	wipi_ids=[]
+	#search for wipi adapters that are available
+	for line in stdout_list:
+		line=line.strip()
+		match=re.search('wipi(\d+)', line)
+		if match:
+			wipi_ids.append(int(match.group(1)))
+	return wipi_ids
